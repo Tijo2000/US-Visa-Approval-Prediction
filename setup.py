c162cbf0f3afa3ede13041c26224739e072ff371
Your Name <[you@example.com])>
2024-04-28 00:55:08 +0530

    Updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Tijo",
+    author_email="thomastijo2000@gmail.com",
+    packages=find_packages()
+)
